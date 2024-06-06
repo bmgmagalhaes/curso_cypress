b5bad4c813337fd0d992659d5af5d6622c71f80d
Bruno Magalhães Gonçalves <bm.goncalves@gmail.com>
2024-06-05 23:15:36 -0300

    Correção do teste das mensagens de erro do cadastro incorreto

--- cypress/e2e/cadastro-incorreto.cy.js
+++ cypress/e2e/cadastro-incorreto.cy.js
@@ -3,10 +3,10 @@ describe('Página de Cadastro', () => {
     cy.visit('https://adopet-frontend-cypress.vercel.app/');
     cy.get('[data-test="register-button"]').click();
     cy.get('[data-test="submit-button"]').click();
-    // cy.contains("É necessário informar um nome");
-    cy.contains("É necessário informar um endereço de email");
-    cy.contains("Crie uma senha");
-    cy.contains("Repita a senha criada acima");
+    // cy.contains("É necessário informar um nome").should('be.visible');;
+    cy.contains("É necessário informar um endereço de email").should('be.visible');
+    cy.contains("Crie uma senha").should('be.visible');;
+    cy.contains("Repita a senha criada acima").should('be.visible');;
     
   })
 })
