2cd398353ca6eafa2f24849e53aa6fbf06694b22
Bruno Magalhães Gonçalves <bm.goncalves@gmail.com>
2024-06-08 17:03:42 -0300

    Omitindo dados sensíveis

--- cypress/e2e/api-mensagem.cy.js
+++ cypress/e2e/api-mensagem.cy.js
@@ -2,11 +2,11 @@
 
 describe('Api Adopet', () => {
     
-    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTcxNzg2NTE5MCwiZXhwIjoxNzE4MTI0MzkwfQ.OXd69Iq_RkU_ISPf6BOQvQNl3JPkYDJTpq7AJYw3t0o`
+    // const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTcxNzg2NTE5MCwiZXhwIjoxNzE4MTI0MzkwfQ.OXd69Iq_RkU_ISPf6BOQvQNl3JPkYDJTpq7AJYw3t0o`
 
     it('Mensagens da API', () => {
         cy.request({
-            headers: {authorization},
+            headers: Cypress.env(),
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-7112-415b-95d2-07904b0d1a1c'
         }).then( (res) => {
--- cypress/e2e/dados-perfil.cy.js
+++ cypress/e2e/dados-perfil.cy.js
@@ -1,13 +1,12 @@
 /// <reference types="cypress" />
 
 describe('Página do Perfil', () => {
-    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTcxNzg2OTIzMSwiZXhwIjoxNzE4MTI4NDMxfQ._mSV2EzpqZ0hnrc-M3c5jIE8RANUd2nUrpzIIEXFfq4`
     
     it('Verificar se o nome do usuário é exibido na página do perfil', () => {
 
         cy.request({
             url: 'https://adopet-api-i8qu.onrender.com/adotante/perfil/11643cd6-7112-415b-95d2-07904b0d1a1c',
-            headers: {authorization},
+            headers: Cypress.env(),
             method: 'GET'
         }).then ( (res) => {
             expect(res.status).to.be.equal(200)
